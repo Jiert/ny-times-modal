8f8d2f59c3f1cd48d5a1d1cbb25dac7d7acc11cb
Jared Easterday <easterday@planet.com>
2018-10-15 09:57:34 -0700

    initial commit

---
+++ ny-times.js
@@ -0,0 +1,5 @@
+const modalClass = '.nytc---modal-window---windowContainer';
+const modal = document.querySelector(modalClass);
+
+modal.parentElement.innerHTML = '';
+document.querySelector('body').style.overflow = 'scroll'
